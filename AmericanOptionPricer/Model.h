93c0a8d488207204c581d72d480c7fe1cb6980ee
mmatthew <takeknock@gmail.com>
2020-01-05 22:57:34 +0900

    feat: add Model base class

--- AmericanOptionPricer/Model.h
+++ AmericanOptionPricer/Model.h
@@ -1,8 +1,10 @@
 #pragma once
 #include <vector>
+#include <memory>
 
 #include "Time.h"
 #include "SampleDef.h"
+#include "Scenario.hpp"
 
 template <typename T>
 class Model {
@@ -15,5 +17,22 @@ public:
         const std::vector<Time>& prdTimeline,
         const std::vector<SampleDef>& prdDefline) = 0;
 
+    virtual size_t simDim() const = 0;
+    
+    virtual void generatePath(
+        const std::vector<double>& gaussVec,
+        Scenario<T>& path) = 0;
+
+    virtual std::unique_ptr<Model<T>> clone() const = 0;
+
+    virtual ~Model(){}
+
+    virtual const std::vector<T*>& parameters() = 0;
+
+    virtual const std::vector<std::string>& parameterLabels() const = 0;
+
+    size_t numParams() const {
+        return const_cast<Model*>(this)->parameters().size();
+    }
 
 };
